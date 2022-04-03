49dc041fc268af9ddfcbfadde1d9af94d37fbe2a
Fernando Aban <88845454+FernandoAbanXcaret@users.noreply.github.com>
2022-04-03 17:52:06 -0500

    algo

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  console.log("algo1")
+  console.log("algo2")
   return (
     <div className={styles.container}>
       <Head>
